964f4027450253c9a1e2341d79d5da115d6e2e79
marteoma <marteomasoyyo@gmail.com>
2018-04-30 15:33:37 -0500

    Chat completo, TODO: ponerle estilos a absolutamente todo, no parece chat

--- src/main/webapp/src/js/chat.js
+++ src/main/webapp/src/js/chat.js
@@ -12,49 +12,51 @@ let database = defaultApp.database();
 
 
 function openChat(rol, id) {
-    document.getElementById('content')
-            .innerHTML += `<div id="chat">
-                                <div id="close">X</div>  
-    
-                                <div class="historical" id="${rol}-historial-${id}"></div>
-    
-                                <form>
-                                    <input type="text" id="mensaje" required>
-                                    <input type="submit" id="${id}" value="enviar"> 
-                                </form>
-                            </div>`;
-    let path = '/mensajes/' + rol + '/' + id + '/';
-    
-    //EVENTO DE LECTURA DE DATOS
-    let messages = database.ref().child('mensajes/'+rol+'/'+id);
-    messages.on('value', (snapshot) => {
-        let data = snapshot.val();
-        let keys = Object.keys(data);
-        let div = document.getElementById(rol+'-historial-'+id);
-        for (let i = 0; i < keys.length; i++) {
-            let k = keys[i];
-            div.innerHTML += `<div class="message">${data[k].mensaje}</div>`;
-        }
-    });
-    
-    //Evento cerrar form
-    document.getElementById('close').addEventListener('click', () => {
-        document.getElementById('chat').remove();
-    });
-    
-    //SUBMIT mssage
-    document.getElementById(id).addEventListener("click", () => {
-        //SUBE LOS DATOS        
-        let postKey = database.ref().child('mensajes').push().key;
-        let ups = {};
-        ups[path + postKey] = {
-            mensaje: document.getElementById('mensaje').value,
-            fecha: new Date().toLocaleString()
-        };
-        database.ref().update(ups);
-        document.getElementById(rol+'-historial-'+id).innerHTML = '';
-    });
+    let newId = rol + '-' + id;
+    if (document.getElementById(newId) === null) {
+        let path = '/mensajes/' + rol + '/' + id + '/';
+        document.getElementById('content')
+                .innerHTML += `<div class="chat" id="${newId}">
+                                    <div class="close" onclick="closeListener(event)">X</div>  
+
+                                    <div class="historical" id="${rol}-historial-${id}"></div>
+
+                                    <form class="form-message">
+                                        <input type="text" id="mensaje" required>
+                                        <input type="submit" id="${id}" value="enviar"> 
+                                    </form>
+                                </div>`;
+
+
+        //EVENTO DE LECTURA DE DATOS
+        let messages = database.ref().child('mensajes/' + rol + '/' + id);
+        messages.on('value', (snapshot) => {
+            let data = snapshot.val();
+            let keys = Object.keys(data);
+            let div = document.getElementById(rol + '-historial-' + id);
+            for (let i = 0; i < keys.length; i++) {
+                let k = keys[i];
+                div.innerHTML += `<div class="message sender-${data[k].sender}">${data[k].mensaje}</div>`;
+            }
+        });
+
+        //SUBMIT mssage
+        document.getElementById(id).addEventListener("click", () => {
+            //SUBE LOS DATOS        
+            let postKey = database.ref().child('mensajes').push().key;
+            let ups = {};
+            ups[path + postKey] = {
+                mensaje: document.getElementById('mensaje').value,
+                fecha: new Date().toLocaleString(),
+                sender: 1
+            };
+            database.ref().update(ups);
+            document.getElementById(rol + '-historial-' + id).innerHTML = '';
+        });
+    }
 }
 
-
+function closeListener(e) {
+    e.target.parentNode.remove();
+}
 
